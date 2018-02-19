3dfb487e23bc5663935d37d39214d28d4eb2798d
kameshwar <kamtasker@gmail.com>
2018-02-19 14:08:23 +0530

    added validation to article modal   and addded routes , controller and template(added form in the template) for artilcles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
-class Article <  ApplicationRecord::Base
+class Article <  ActiveRecord::Base
+
+  validates :title ,presence:true , length: { minimum:3 , maximun:30 }
+  validates :description ,presence:true , length: { minimum:10, maximun:300 }
 
 
 end
