217f1e2744e1d387d1dd46386cfecde61ee39b1e
kameshwar <kamtasker@gmail.com>
2018-02-23 15:24:29 +0530

    added styles to error and flash msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice]="Article is successfully created."
+      flash[:success]="Article is successfully created."
       redirect_to article_path(@article)
     else
       render 'new'
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] ="Article was successfully updated"
+      flash[:success] ="Article was successfully updated"
       redirect_to article_path(@article)
     else
       render "edit"
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article deleted successfully"
+    flash[:danger] = "Article deleted successfully"
     redirect_to articles_path
 
   end
