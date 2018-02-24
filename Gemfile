3d2705c1b5358f3de48c90a12d91bbd9b0771751
kameshwar <kamtasker@gmail.com>
2018-02-24 22:10:11 +0530

    add password authentication  to user and gem file for that

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
+gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224125446) do
+ActiveRecord::Schema.define(version: 20180224141635) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,11 +20,7 @@ ActiveRecord::Schema.define(version: 20180224125446) do
     t.integer "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'String' for column 'password_digest'
 
 end
