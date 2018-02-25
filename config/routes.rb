1b77f57d3def0296b9bb8d60fe6fcf27f51d5948
kameshwar <kamtasker@gmail.com>
2018-02-25 12:39:07 +0530

    added user controller and add users to viiew and add new page to users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root "pages#home" 
   get "about",to:"pages#about"
+
+  get "signup" , to:"users#new"
+  resources :users, expect: [:new]
   
   resources :articles
 end
