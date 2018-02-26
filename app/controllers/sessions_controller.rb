162f749db5faa32c5043f073a3cde0026f9663ab
kameshwar <kamtasker@gmail.com>
2018-02-26 10:40:30 +0530

    some improvment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
     end
 
     def create
-      user = User.find_by(email:params[:session][:emali].downcase)
+      user = User.find_by(email:params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
-        session[:user-id] = user.id
+        session[:user_id] = user.id
         flash[:success] ="you have successfully looged in"
         redirect_to user_path(user)
       else
